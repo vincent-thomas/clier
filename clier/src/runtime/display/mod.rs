ad9abbcaabababa4c67aab2d529f9e3e09e88425
Vincent Thomas <77443389+vincent-thomas@users.noreply.github.com>
2024-05-17 21:06:48 +0200

    not owned displayer

--- clier/src/runtime/display/mod.rs
+++ clier/src/runtime/display/mod.rs
@@ -8,26 +8,29 @@ pub enum Displayer {
   /// .
   Error,
   /// .
-  Todo
+  Todo,
+  /// .
+  Success,
 }
 
 impl Displayer {
-  fn format<'a>(self) -> StyledObject<&'a str> {
+  fn format<'a>(&self) -> StyledObject<&'a str> {
     match self {
       Self::Info => style(" INFO ").on_blue(),
-      Self::Error => style(" ERROR ").on_red().bold(),
-      Self::Todo => style(" TODO ").on_yellow()
+      Self::Error => style(" ERROR ").on_red(),
+      Self::Todo => style(" TODO ").on_yellow(),
+      Self::Success => style(" SUCCESS ").on_green(),
     }
   }
   /// Write shit
-  pub fn write(self, text: impl Into<String>) {
+  pub fn write(&self, text: impl Into<String>) {
     let term = Term::stdout();
     let what = f!("{} {}", self.format(), text.into());
     let _ = term.write_line(what.as_str());
   }
 
   /// Write shit
-  pub fn write_err(self, text: impl Into<String>) {
+  pub fn write_err(&self, text: impl Into<String>) {
     let term = Term::stderr();
     let what = f!("{} {}", self.format(), text.into());
     let _ = term.write_line(what.as_str());
